b8eb126bbf66250bf0eead298afbca6abb063398
YounaChen <unitypro99@gmail.com>
2024-06-20 22:00:34 +0800

    Add games status & Show FPS

--- main.lua
+++ main.lua
@@ -1,5 +1,15 @@
 local love = require "love"
 
+local game = {
+    state = {
+        menu = true,
+        paused = false,
+        running = false,
+        ended = false
+    }
+}
+
+
 -- 遊戲玩家的光標
 local player = { 
     radius = 20,
@@ -10,6 +20,7 @@ local player = {
 
 function love.load()
     love.window.setTitle("Save the ball")
+    love.mouse.setVisible(false)
 end
 
 function love.update()
@@ -18,5 +29,14 @@ end
 
 
 function love.draw()
-    love.graphics.circle("fill", player.x, player.y, player.radius)
+    love.graphics.printf("FPS: " .. love.timer.getFPS(), 
+                        love.graphics.newFont(16), 10, love.graphics.getHeight() - 30, love.graphics.getWidth())
+
+    if game.state["running"] then
+        love.graphics.circle("fill", player.x, player.y, player.radius)
+    end
+
+    if not game.state["running"] then
+        love.graphics.circle("fill", player.x, player.y, player.radius/2)
+    end
 end
