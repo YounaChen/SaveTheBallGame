b6da0971468de858b6947efbd86ccfc20873343f
YounaChen <unitypro99@gmail.com>
2024-06-20 21:37:52 +0800

    Create player's cursor

---
+++ main.lua
@@ -0,0 +1,22 @@
+local love = require "love"
+
+-- 遊戲玩家的光標
+local player = { 
+    radius = 20,
+    x = 30,
+    y = 30
+}
+
+
+function love.load()
+    love.window.setTitle("Save the ball")
+end
+
+function love.update()
+    player.x, player.y = love.mouse.getPosition()
+end
+
+
+function love.draw()
+    love.graphics.circle("fill", player.x, player.y, player.radius)
+end
