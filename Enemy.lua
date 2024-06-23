9f18b75571bb0d29d166960d922617d4ab6fe4c3
YounaChen <unitypro99@gmail.com>
2024-06-23 16:07:02 +0800

    Create enemy (running)

--- Enemy.lua
+++ Enemy.lua
@@ -1,20 +1,39 @@
 local love = require "love"
 
 function Enemy()
+    local dice = math.random(1, 4)
+    local _X, _y
+    local _radius = 20
+
+    if dice == 1 then
+        _x = math.random(_radius, love.graphics.getWidth())
+        _y = -_radius * 4
+    elseif dice == 2 then
+        _x = -_radius * 4
+        _y = math.random(_radius, love.graphics.getHeight())
+    elseif dice == 3 then
+        _x = math.random(_radius, love.graphics.getWidth())
+        _y = love.graphics.getHeight() + (-_radius * 4)
+    else
+        _x = love.graphics.getWidth() + (-_radius * 4)
+        _y = math.random(_radius, love.graphics.getHeight())
+    end
+
     return {
         level = 1,
-        redius = 20,
-        x = -10,
-        y = -50,
+        radius = _radius,
+        x = _x,
+        y = _y,
 
         move = function (self, player_x, player_y)
             -- When the enemy is on the left side of the player, the enemy should move right to approach the player. 
+            -- move to in player x pos
             if player_x - self.x > 0 then
                 self.x = self.x + self.level
             elseif player_x - self.x < 0 then
                 self.x = self.x - self.level
             end
-
+            -- move to in player y pos
             if player_y - self.y > 0 then
                 self.y = self.y + self.level
             elseif player_y - self.y < 0 then
@@ -22,11 +41,13 @@ function Enemy()
             end
         end,
 
+        -- Draw enemy
         draw = function (self)
-            love.graphics.setColor(255/255, 125/255 , 122/255)
+            love.graphics.setColor(1, 0.5 , 0.5)
             love.graphics.circle("fill", self.x, self.y, self.radius)
+            -- reset the color back to white
             love.graphics.setColor(1, 1, 1)
-        end
+        end,
     }
 
 
--- main.lua
+++ main.lua
@@ -1,13 +1,16 @@
 local love = require "love"
+local enemy = require "Enemy"
+
+math.randomseed(os.time())
 
 local game = {
     difficulty = 1,
     state = {
-        menu = true,
+        menu = false,
         paused = false,
-        running = false,
+        running = true,
         ended = false
-    }
+    },
 }
 
 -- Player's cursor
@@ -17,25 +20,32 @@ local player = {
     y = 30
 }
 
-local enemies = {
-
-}
+local enemies = {}
 
 function love.load()
     love.window.setTitle("Save the ball")
     love.mouse.setVisible(false)
+
+    table.insert(enemies, 1, enemy())
 end
 
-function love.update()
+function love.update(dt)
     player.x, player.y = love.mouse.getPosition()
+
+    for i = 1, #enemies do
+        enemies[i]:move(player.x, player.y)
+    end
 end
 
 
 function love.draw()
-    love.graphics.printf("FPS: " .. love.timer.getFPS(), 
-                        love.graphics.newFont(16), 10, love.graphics.getHeight() - 30, love.graphics.getWidth())
+    love.graphics.printf("FPS: " .. love.timer.getFPS(), love.graphics.newFont(16), 10, love.graphics.getHeight() - 30, love.graphics.getWidth())
 
     if game.state["running"] then
+        for i = 1, #enemies do
+            enemies[i]:draw()
+        end
+
         love.graphics.circle("fill", player.x, player.y, player.radius)
     end
 
